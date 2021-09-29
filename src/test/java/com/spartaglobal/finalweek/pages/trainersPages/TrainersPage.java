b060fb87d89b9a8190e98f81b5f085368ef07a0f
nirel-warde <nirelwarde@gmail.com>
2021-09-29 16:30:54 +0100

    TrainersPage empty methods

--- src/test/java/com/spartaglobal/finalweek/pages/trainersPages/TrainersPage.java
+++ src/test/java/com/spartaglobal/finalweek/pages/trainersPages/TrainersPage.java
@@ -1,12 +1,17 @@
 package com.spartaglobal.finalweek.pages.trainersPages;
 
+import com.spartaglobal.finalweek.base.TestBase;
 import com.spartaglobal.finalweek.interfaces.URLable;
 import com.spartaglobal.finalweek.pages.NavTemplate;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class TrainersPage extends NavTemplate implements URLable {
 
     public TrainersPage() {
+        TestBase.initialisation();
         PageFactory.initElements(webDriver, this);
     }
 
@@ -14,5 +19,120 @@ public class TrainersPage extends NavTemplate implements URLable {
     public String getURL() {
         return webDriver.getCurrentUrl();
     }
+
+    public List<WebElement> getAllTrainersFirstNameElements() {
+        return null;
+    }
+
+    public List<WebElement> getAllTrainersLastNameElements() {
+
+        return null;
+
+    }
+
+    public List<WebElement> getAllTrainerElements() {
+        return null;
+
+    }
+
+    public List<WebElement> getAllTrainerColourElements() {
+        return null;
+
+    }
+
+    public List<String> getAllTrainersFirstName() {
+        return null;
+
+    }
+
+    public List<String> getAllTrainersLastName() {
+        return null;
+
+    }
+
+    public List<String> getAllTrainerColour() {
+        return null;
+
+    }
+
+    public List<WebElement> getTrainerFirstNameElement(int id) {
+        return null;
+
+    }
+
+    public List<WebElement> getTrainerLastNameElement(int id) {
+        return null;
+
+    }
+
+    public List<WebElement> getTrainerColourElement(int id) {
+        return null;
+
+    }
+
+    public String getTrainerFirstName(int rowID) {
+        return null;
+
+    }
+
+    public String getTrainerLastName(int rowID) {
+        return null;
+
+    }
+    public String getTrainerColour(int rowID) {
+        return null;
+
+    }
+
+    public AddTrainersPage clickAddTrainer() {
+        return null;
+
+    }
+
+    public EditTrainersPage clickEditTrainer() {
+        return null;
+
+    }
+
+    public void clickDeleteTrainer() {
+
+
+    }
+
+    public boolean isTrainerDeleted() {
+        return false;
+
+    }
+
+    public boolean isTrainerFirstNameValid() {
+        return false;
+
+    }
+
+    public boolean isTrainerLastNameValid() {
+        return false;
+
+    }
+
+    public boolean areAllTrainersFirstNamesValid() {
+        return false;
+
+    }
+
+    public boolean areAllTrainersLastNamesValid() {
+        return false;
+
+    }
+
+    public boolean areAllColoursUnique() {
+        return false;
+
+    }
+
+    public boolean areAllFieldsPassedOnToEditTrainersPage() {
+        return false;
+
+    }
+
 
 }
