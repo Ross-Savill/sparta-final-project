b315d67d23e4b4c8ec7d1a8a1894c8bd80a0422d
joshuamc360 <joshuamc360@gmail.com>
2021-09-30 12:29:22 +0100

    Account page and tests completed

--- src/test/java/com/spartaglobal/finalweek/pages/AccountPage.java
+++ src/test/java/com/spartaglobal/finalweek/pages/AccountPage.java
@@ -1,12 +1,70 @@
 package com.spartaglobal.finalweek.pages;
 
+import com.spartaglobal.finalweek.base.TestBase;
 import com.spartaglobal.finalweek.interfaces.URLable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import static com.spartaglobal.finalweek.base.TestBase.webDriver;
 
-public class AccountPage extends NavTemplate implements URLable {
+
+
+public class AccountPage implements URLable {
+
+    @FindBy(id = "newPassword")
+    WebElement newPasswordTextField;
+    @FindBy(id = "confirmPassword")
+    WebElement confirmPasswordTextField;
+    @FindBy(id = "submitPassword")
+    WebElement submitButton;
 
     public AccountPage() {
         PageFactory.initElements(webDriver, this);
+    }
+
+    public void enterNewPassword(String password){
+        newPasswordTextField.sendKeys(password);
+    }
+
+    public void enterConfirmPassword(String password){
+        confirmPasswordTextField.sendKeys(password);
+    }
+
+    public String getNewPasswordTextField() {
+        return webDriver.findElement(By.id("newPassword")).getAttribute("value");
+    }
+
+    public String getConfirmPasswordTextField() {
+        return webDriver.findElement(By.id("confirmPassword")).getAttribute("value");
+    }
+
+    public boolean submitSuccessful(){
+        submitButton.click();
+        return submitButton.isEnabled();
+    }
+
+    public boolean isErrorMessageDisplayed(String password, String differentPassword){
+        enterNewPassword(password);
+        enterConfirmPassword(differentPassword);
+        WebElement errorMessage = webDriver.findElement(By.id("message"));
+
+        return errorMessage.isDisplayed();
+    }
+
+    public boolean isMatchingPassword(){
+        String newPasswordTextFieldAttribute = newPasswordTextField.getAttribute("value");
+        String confirmPasswordTextFieldAttribute = confirmPasswordTextField.getAttribute("value");
+
+        return (newPasswordTextFieldAttribute.equals(confirmPasswordTextFieldAttribute));
+    }
+
+    public boolean areBothFieldsFilled(){
+        String newPasswordTextFieldAttribute = newPasswordTextField.getAttribute("value");
+        String confirmPasswordTextFieldAttribute = confirmPasswordTextField.getAttribute("value");
+
+        return (!newPasswordTextFieldAttribute.isEmpty()&&!confirmPasswordTextFieldAttribute.isEmpty());
     }
 
     @Override
