90046eab9f137346a538d284d8a2e82c785d3dad
Sogurung16 <Sogurung16@gmail.com>
2021-09-30 12:29:42 +0100

    implemented get, enter, isEmpty and isBlank for the course name field. clickSubmit and getEmptyErrorMessage also implemented

--- src/test/java/com/spartaglobal/finalweek/pages/coursePages/AddCoursePage.java
+++ src/test/java/com/spartaglobal/finalweek/pages/coursePages/AddCoursePage.java
@@ -2,9 +2,16 @@ package com.spartaglobal.finalweek.pages.coursePages;
 
 import com.spartaglobal.finalweek.interfaces.URLable;
 import com.spartaglobal.finalweek.pages.NavTemplate;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import static com.spartaglobal.finalweek.base.TestBase.webDriver;
 
-public class AddCoursePage extends NavTemplate implements URLable {
+public class AddCoursePage implements URLable {
+    private @FindBy (id = "course_name")
+    WebElement courseName;
+    private @FindBy (id = "inputButton")
+    WebElement submitButton;
 
     public AddCoursePage() {
         PageFactory.initElements(webDriver, this);
@@ -15,4 +22,32 @@ public class AddCoursePage extends NavTemplate implements URLable {
         return webDriver.getCurrentUrl();
     }
 
+    public WebElement getCourseNameElement(){
+        return this.courseName;
+    }
+
+    public String getCourseName(){
+        return getCourseNameElement().getAttribute("value");
+    }
+
+    public void enterCourseName(String courseName){
+        getCourseNameElement().click();
+        getCourseNameElement().sendKeys(courseName);
+    }
+
+    public boolean isCourseNameEmpty(){
+        return getCourseName().isEmpty();
+    }
+
+    public boolean isCourseNameBlank(){
+        return getCourseName().isBlank();
+    }
+
+    public boolean getEmptyErrorMessage(){
+        return Boolean.getBoolean(getCourseNameElement().getAttribute("required"));
+    }
+
+    public void clickSubmit(){
+        submitButton.click();
+    }
 }
