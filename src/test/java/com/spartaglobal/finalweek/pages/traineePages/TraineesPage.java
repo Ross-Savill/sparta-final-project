7bd3016b0f75ebc24c74ec1e233a7c970958c83c
Ross Savill <rosssavill252@gmail.com>
2021-10-01 13:58:26 +0100

    Ross half way through adding methods to Trainees Page

--- src/test/java/com/spartaglobal/finalweek/pages/traineePages/TraineesPage.java
+++ src/test/java/com/spartaglobal/finalweek/pages/traineePages/TraineesPage.java
@@ -1,20 +1,28 @@
 package com.spartaglobal.finalweek.pages.traineePages;
 
+import com.spartaglobal.finalweek.base.TestBase;
 import com.spartaglobal.finalweek.interfaces.URLable;
 import com.spartaglobal.finalweek.pages.NavTemplate;
 import com.spartaglobal.finalweek.pages.courseInfoPages.EditCourseTypePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class TraineesPage extends NavTemplate implements URLable {
+import static com.spartaglobal.finalweek.base.TestBase.webDriver;
+
+public class TraineesPage implements URLable {
 
     @FindBy(id = "traineePageLink") WebElement addTraineeButton;
     @FindBy(id = "accordion-button") WebElement courseFilterDropDownButton;
     @FindBy(id = "course") List<WebElement> coursesWithinFilter;
-    @FindBy() List<WebElement> allTrainees; // How to get a list of all Trainees? Just different row IDs
     @FindBy(css = "#\30 row > td:nth-child(4) > div:nth-child(1) > button:nth-child(1) > a:nth-child(1)") WebElement editTraineeButton;
     @FindBy(css = "#\\30 row > td:nth-child(5) > div:nth-child(1) > button:nth-child(1) > a:nth-child(1)") WebElement deleteTraineeButton;
     @FindBy(css = "#\\30 row > td:nth-child(6) > div:nth-child(1) > button:nth-child(1) > a:nth-child(1)") WebElement addQualityGateButton;
@@ -24,65 +32,183 @@ public class TraineesPage extends NavTemplate implements URLable {
         PageFactory.initElements(webDriver, this);
     }
 
-//    public List<WebElement> getAllTraineesElements() {
-//
-//    }
+    public List<WebElement> getAllTraineesElements() {
+        WebElement traineesTable;
+        WebElement tableBody;
+        List<WebElement> allTraineeRows;
+        traineesTable = webDriver.findElement(By.id("traineeTable"));
+        tableBody = traineesTable.findElement(By.tagName("tbody"));
+        WebDriverWait wait = new WebDriverWait(webDriver,10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("tr")));
+        allTraineeRows = tableBody.findElements(By.tagName("tr"));
+        return allTraineeRows;
+    }
 
-//    public List<WebElement> getAllTraineesFirstNameElements() {
-//
-//    }
+    public List<WebElement> getAllTraineesFirstNameElements() {
+        List<WebElement> allFirstNameElements = new ArrayList<>();
+        List<WebElement> allTraineeRows = getAllTraineesElements();
 
-//    public List<WebElement> getAllTraineesLastNameElements() {
-//
-//    }
+        for(int i = 0; i < allTraineeRows.size(); i++) {
+            WebElement firstNameElement = allTraineeRows.get(i).findElement(By.id(i+"name"));
+            allFirstNameElements.add(firstNameElement);
+        }
+        return allFirstNameElements;
+    }
 
-//    public List<WebElement> getAllTraineesQualityGateStatusElements() {
-//
-//    }
+    public List<WebElement> getAllTraineesLastNameElements() {
+        List<WebElement> allLastNameElements = new ArrayList<>();
+        List<WebElement> allTraineeRows = getAllTraineesElements();
 
-//    public List<WebElement> getAllTraineesFirstNames() {
-//
-//    }
+        for(int i = 0; i < allTraineeRows.size(); i++) {
+            WebElement lastNameElement = allTraineeRows.get(i).findElement(By.id(i+"surname"));
+            allLastNameElements.add(lastNameElement);
+        }
+        return allLastNameElements;
+    }
 
-//    public List<WebElement> getAllTraineesLastNames() {
-//
-//    }
+    public List<WebElement> getAllTraineesQualityGateStatusElements() {
+        List<WebElement> allQualityGateStatusElements = new ArrayList<>();
+        List<WebElement> allTraineeRows = getAllTraineesElements();
 
-//    public List<WebElement> getAllTraineesQualityGateStatus() {
-//
-//    }
+        for(int i = 0; i < allTraineeRows.size(); i++) {
+            WebElement qualityGateStatusElement = allTraineeRows.get(i).findElement(By.id(i+"qgs"));
+            allQualityGateStatusElements.add(qualityGateStatusElement);
+        }
+        return allQualityGateStatusElements;
+    }
 
-//    public WebElement getTraineeElement(int rowID) {
-//
-//    }
+    public List<String> getAllTraineesFirstNames() {
+        List<String> allFirstNameStrings = new ArrayList<>();
+        List<WebElement> allTraineeRows = getAllTraineesElements();
 
-//    public WebElement getTraineeFirstNameElement(int rowID) {
-//
-//    }
+        for(int i = 0; i < allTraineeRows.size(); i++) {
+            WebElement firstNameString = allTraineeRows.get(i).findElement(By.id(i+"name"));
+            allFirstNameStrings.add(firstNameString.getText());
+        }
+        System.out.println(allFirstNameStrings);
+        return allFirstNameStrings;
+    }
 
-//    public WebElement getTraineeLastNameElement(int rowID) {
-//
-//    }
+    public List<String> getAllTraineesLastNames() {
+        List<String> allLastNameStrings = new ArrayList<>();
+        List<WebElement> allTraineeRows = getAllTraineesElements();
 
-//    public String getTraineeFirstName(int rowID) {
-//
-//    }
+        for(int i = 0; i < allTraineeRows.size(); i++) {
+            WebElement firstNameString = allTraineeRows.get(i).findElement(By.id(i+"surname"));
+            allLastNameStrings.add(firstNameString.getText());
+        }
+        return allLastNameStrings;
+    }
 
-//    public String getTraineeLastName(int rowID) {
-//
-//    }
+    public List<String> getAllTraineesQualityGateStatusStrings() {
+        List<String> allQualityGateStrings = new ArrayList<>();
+        List<WebElement> allTraineeRows = getAllTraineesElements();
 
-//    public WebElement getTraineeQualityGateStatusElement(int rowID) {
-//
-//    }
+        for(int i = 0; i < allTraineeRows.size(); i++) {
+            WebElement qualityGateString = allTraineeRows.get(i).findElement(By.id(i+"qgs"));
+            allQualityGateStrings.add(qualityGateString.getText());
+        }
+        return allQualityGateStrings;
+    }
 
-//    public String getTraineeQualityGateStatus(int rowID) {
-//
-//    }
+    public WebElement getTraineeElement(String rowID) {
+        List<WebElement> allTraineeRows = getAllTraineesElements();
+        for(WebElement row : allTraineeRows) {
+            if(row.getAttribute("id").equals(rowID)) {
+                return row;
+            }
+        }
+        return null;
+    }
 
-//    public WebElement getQualityGateHistoryDetailsElement(int rowID) {
-//
-//    }
+    public WebElement getTraineeFirstNameElement(String rowID) {
+        List<WebElement> allTraineeRows = getAllTraineesElements();
+        for(WebElement row : allTraineeRows) {
+            if(row.getAttribute("id").equals(rowID)) {
+                return row.findElement(By.id(allTraineeRows.indexOf(row)+"name"));
+            }
+        }
+        return null;
+    }
+
+    public WebElement getTraineeLastNameElement(String rowID) {
+        List<WebElement> allTraineeRows = getAllTraineesElements();
+        for(WebElement row : allTraineeRows) {
+            if(row.getAttribute("id").equals(rowID)) {
+                return row.findElement(By.id(allTraineeRows.indexOf(row)+"surname"));
+            }
+        }
+        return null;
+    }
+
+    public WebElement getTraineeQualityGateStatusElement(String rowID) {
+        List<WebElement> allTraineeRows = getAllTraineesElements();
+        for(WebElement row : allTraineeRows) {
+            if(row.getAttribute("id").equals(rowID)) {
+                return row.findElement(By.id(allTraineeRows.indexOf(row)+"qgs"));
+            }
+        }
+        return null;
+    }
+
+    public String getTraineeFirstName(String rowID) {
+        List<WebElement> allTraineeRows = getAllTraineesElements();
+        for(WebElement row : allTraineeRows) {
+            if(row.getAttribute("id").equals(rowID)) {
+                return row.findElement(By.id(allTraineeRows.indexOf(row)+"name")).getText();
+            }
+        }
+        return null;
+    }
+
+    public String getTraineeLastName(String rowID) {
+        List<WebElement> allTraineeRows = getAllTraineesElements();
+        for(WebElement row : allTraineeRows) {
+            if(row.getAttribute("id").equals(rowID)) {
+                return row.findElement(By.id(allTraineeRows.indexOf(row)+"surname")).getText();
+            }
+        }
+        return null;
+    }
+
+    public String getTraineeQualityGateStatus(String rowID) {
+        List<WebElement> allTraineeRows = getAllTraineesElements();
+        for(WebElement row : allTraineeRows) {
+            if(row.getAttribute("id").equals(rowID)) {
+                return row.findElement(By.id(allTraineeRows.indexOf(row)+"qgs")).getText();
+            }
+        }
+        return null;
+    }
+
+    public List<WebElement> getQualityGateHistoryDetailsElements(String rowID) {
+
+        WebElement traineesTable;
+        WebElement tableBody;
+        List<WebElement> allTraineeRows;
+        traineesTable = webDriver.findElement(By.id("traineeTable"));
+        tableBody = traineesTable.findElement(By.tagName("tbody"));
+        WebDriverWait wait = new WebDriverWait(webDriver,10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("tr")));
+        allTraineeRows = tableBody.findElements(By.tagName("tr"));
+
+        WebElement qualityGateTable;
+        List<WebElement> totalTableDataElements;
+        List<WebElement> qualityGateData = new ArrayList<>();
+        for(WebElement row : allTraineeRows) {
+            if(row.getAttribute("id").equals(rowID)) {
+                row.findElement(By.tagName("td")).click();
+                qualityGateTable = tableBody.findElement(By.className("footable-details"));
+                totalTableDataElements = qualityGateTable.findElements(By.tagName("td"));
+                for(WebElement element : totalTableDataElements) {
+                    if(element.getText() != null) {
+                        System.out.println("STARTING HERE " + element.getText());
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
 //    public String getQualityGateHistoryDetails(int rowID) {
 //
--- src/test/java/com/spartaglobal/finalweek/tests/traineeTests/TraineesTests.java
+++ src/test/java/com/spartaglobal/finalweek/tests/traineeTests/TraineesTests.java
@@ -1,14 +1,48 @@
 package com.spartaglobal.finalweek.tests.traineeTests;
 
+import com.spartaglobal.finalweek.base.TestBase;
+import com.spartaglobal.finalweek.pages.LoginPage;
 import com.spartaglobal.finalweek.pages.NavTemplate;
+import com.spartaglobal.finalweek.pages.SchedulerPage;
+import com.spartaglobal.finalweek.pages.traineePages.TraineesPage;
+import com.spartaglobal.finalweek.util.PropertiesLoader;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class TraineesTests extends NavTemplate {
 
+    private static String username = PropertiesLoader.getProperties().getProperty("Username");
+    private static String password = PropertiesLoader.getProperties().getProperty("Password");
+    private LoginPage loginPage;
+    private SchedulerPage schedulerPage;
+    private TraineesPage traineesPage;
+
     @BeforeEach
     public void setup() {
+        TestBase.initialisation();
+        PageFactory.initElements(webDriver, this);
+        loginPage = new LoginPage();
+        schedulerPage = new SchedulerPage();
+        traineesPage = new TraineesPage();
+        loginPage.login(username, password);
+        PageFactory.initElements(webDriver, schedulerPage);
+        schedulerPage.goToTraineesPage();
+        PageFactory.initElements(webDriver, traineesPage);
+    }
 
+    @Test
+    public void getAllTrainees() {
+        System.out.println(traineesPage.getQualityGateHistoryDetailsElements("0row"));
     }
 
     @AfterEach
