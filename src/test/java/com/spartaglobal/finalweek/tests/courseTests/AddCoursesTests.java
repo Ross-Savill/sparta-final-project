531ab2a9412abc58109dc863762752f71d9f6242
Sogurung16 <Sogurung16@gmail.com>
2021-10-01 13:54:39 +0100

    implemented tests for trainer start week field

--- src/test/java/com/spartaglobal/finalweek/tests/courseTests/AddCoursesTests.java
+++ src/test/java/com/spartaglobal/finalweek/tests/courseTests/AddCoursesTests.java
@@ -7,6 +7,9 @@ import com.spartaglobal.finalweek.pages.coursePages.AddCoursePage;
 import com.spartaglobal.finalweek.pages.coursePages.CoursePage;
 import com.spartaglobal.finalweek.util.PropertiesLoader;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static com.spartaglobal.finalweek.base.TestBase.webDriver;
 
 public class AddCoursesTests{
@@ -35,12 +38,12 @@ public class AddCoursesTests{
     }
 
     @Nested
-    @DisplayName("Testing Enter and Getter methods for course name fields")
+    @DisplayName("Testing Enter and Getter methods for course name field")
     class GettingAndEnteringCourseNameFields {
 
         @Test
         @DisplayName("Test get course name returns Sentinel value")
-        void testEnterCourseNameField() {
+        void testEnterSentinelCourseNameField() {
             addCoursePage.enterCourseName("Sentinel");
             Assertions.assertEquals("Sentinel", addCoursePage.getCourseName());
         }
@@ -51,11 +54,95 @@ public class AddCoursesTests{
             addCoursePage.enterCourseName("");
             Assertions.assertEquals("", addCoursePage.getCourseName());
         }
+    }
+
+    @Nested
+    @DisplayName("Testing select and getter methods for trainer ID")
+    class GettingAndSelectingTrainerID {
 
         @Test
-        @DisplayName("Test entering no course name returns empty course name")
-        void testNoEntryCourseNameField() {
-            Assertions.assertEquals("", addCoursePage.getCourseName());
+        @DisplayName("Test getting 1st trainer id returns default value 'Mike Wazowski'")
+        void testGettingTrainerIdReturnsDefaultValueMikeWazowski() {
+            Assertions.assertEquals("Mike Wazowski", addCoursePage.getTrainerID(1));
+        }
+
+        @Test
+        @DisplayName("Test selecting 'Mace Windu' trainer id return 'Mace Windu'")
+        void testSelectingMaceWinduTrainerIdReturnMaceWindu() {
+            addCoursePage.selectTrainerID(1, "Mace Windu");
+            Assertions.assertEquals("Mace Windu", addCoursePage.getTrainerID(1));
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"1, Mike Wazowski", "2, Mace Windu", "3, Obi-Wan Kenobi"})
+        @DisplayName("Test adding multiple trainers and getting trainer id")
+        void testMultipleTrainerIds(String row, String trainerId) {
+            int numberOfTrainers = Integer.parseInt(row);
+            for (int i = 1; i < numberOfTrainers; i++){
+                addCoursePage.incrementNumberOfTrainers();
+            }
+            addCoursePage.selectTrainerID(numberOfTrainers, trainerId);
+            Assertions.assertEquals(trainerId, addCoursePage.getTrainerID(numberOfTrainers));
+        }
+
+        @Test
+        @DisplayName("Test getting trainer start week returns default value one")
+        void testGettingTrainerStartWeekReturnsDefaultValueOne() {
+            Assertions.assertEquals(1, Integer.parseInt(addCoursePage.getTrainerStartWeek(1)));
+        }
+
+        @Test
+        @DisplayName("Test enter start week")
+        void testEnterStartWeek() {
+            addCoursePage.enterTrainerStartWeek(1, 12);
+            Assertions.assertEquals(12, Integer.parseInt(addCoursePage.getTrainerStartWeek(1)));
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {"1, 12", "2, 6", "3, 52"})
+        @DisplayName("Test adding multiple trainers and getting trainer id")
+        void testMultipleTrainerStarWeeks(String row, String trainerStartWeek) {
+            int numberOfTrainers = Integer.parseInt(row);
+            int trainerStartWeekNum = Integer.parseInt(trainerStartWeek);
+
+            for (int i = 1; i < numberOfTrainers; i++){
+                addCoursePage.incrementNumberOfTrainers();
+            }
+            addCoursePage.enterTrainerStartWeek(numberOfTrainers, trainerStartWeekNum);
+            Assertions.assertEquals(trainerStartWeekNum, Integer.parseInt(addCoursePage.getTrainerStartWeek(numberOfTrainers)));
+        }
+    }
+
+    @Nested
+    @DisplayName("Testing increment, decrement, Enter and Getter methods for number of trainers field")
+    class GettingAndEnteringNumberOfTrainersFields {
+
+        @Test
+        @DisplayName("Test getting default number of trainers is 1")
+        void testDefaultNumberOfTrainersField() {
+            Assertions.assertEquals(1, Integer.parseInt(addCoursePage.getNumberOfTrainers()));
+        }
+
+        @Test
+        @DisplayName("Test increment  number of trainers")
+        void testIncrementNumberOfTrainerValue() {
+            addCoursePage.incrementNumberOfTrainers();
+            Assertions.assertEquals(2, Integer.parseInt(addCoursePage.getNumberOfTrainers()));
+        }
+
+        @Test
+        @DisplayName("Test decrement number of trainers")
+        void testDecrementNumberOfTrainerValue() {
+            addCoursePage.incrementNumberOfTrainers();
+            addCoursePage.decrementNumberOfTrainers();
+            Assertions.assertEquals(1, Integer.parseInt(addCoursePage.getNumberOfTrainers()));
+        }
+
+        @Test
+        @DisplayName("Test enter number of trainers")
+        void testEnterNumberOfTrainers() {
+            addCoursePage.enterNumberOfTrainers(6);
+            Assertions.assertEquals(6, Integer.parseInt(addCoursePage.getNumberOfTrainers()));
         }
     }
 
@@ -74,6 +161,19 @@ public class AddCoursesTests{
         void nonEmptyCourseNameReturnsFalse() {
             addCoursePage.enterCourseName("Eng-92");
             Assertions.assertFalse(addCoursePage.isCourseNameEmpty());
+        }
+
+        @Test
+        @DisplayName("empty trainer start week returns true")
+        void emptyTrainerStartWeekReturnsTrue() {
+            addCoursePage.getTrainerStartWeekElement(1).clear();
+            Assertions.assertTrue(addCoursePage.isTrainerStartWeekEmpty(1));
+        }
+
+        @Test
+        @DisplayName("empty trainer start week returns false")
+        void emptyTrainerStartWeekReturnsFalse() {
+            Assertions.assertFalse(addCoursePage.isTrainerStartWeekEmpty(1));
         }
     }
 
@@ -96,7 +196,55 @@ public class AddCoursesTests{
         }
     }
 
- /*   @Nested
+    @Nested
+    @DisplayName("Valid entries test")
+    class ValidTests {
+
+        @Test
+        @DisplayName("Valid number of trainers returns true")
+        void validNumberOfTrainersReturnsTrue() {
+            addCoursePage.enterNumberOfTrainers(5);
+            Assertions.assertTrue(addCoursePage.isNumberOfTrainersValid());
+        }
+
+        @Test
+        @DisplayName("invalid low number of trainers returns false")
+        void invalidLowNumberOfTrainersReturnsFalse() {
+            addCoursePage.enterNumberOfTrainers(0);
+            Assertions.assertFalse(addCoursePage.isNumberOfTrainersValid());
+        }
+
+        @Test
+        @DisplayName("invalid high number of trainers returns false")
+        void invalidHighNumberOfTrainersReturnsFalse() {
+            addCoursePage.enterNumberOfTrainers(11);
+            Assertions.assertFalse(addCoursePage.isNumberOfTrainersValid());
+        }
+
+        @Test
+        @DisplayName("Valid number of Trainer Start Week returns true")
+        void validNumberOfTrainerStartWeekReturnsTrue() {
+            addCoursePage.enterTrainerStartWeek(1, 12);
+            Assertions.assertTrue(addCoursePage.isTrainerStartWeekValid(1));
+        }
+
+        @Test
+        @DisplayName("Invalid low number of Trainer Start Week returns false")
+        void InvalidLowNumberOfTrainerStartWeekReturnsFalse() {
+            addCoursePage.enterTrainerStartWeek(1, 0);
+            Assertions.assertFalse(addCoursePage.isTrainerStartWeekValid(1));
+        }
+
+        @Test
+        @DisplayName("Invalid high number of Trainer Start Week returns false")
+        void InvalidHighNumberOfTrainerStartWeekReturnsFalse() {
+            addCoursePage.enterTrainerStartWeek(1, 53);
+            Assertions.assertFalse(addCoursePage.isTrainerStartWeekValid(1));
+        }
+    }
+
+    //TODO(2): Dependant on database having maximum of 1 course.
+    /*@Nested
     @DisplayName("EmptyErrorMessage warning test")
     class EmptyWarningTests {
 
@@ -105,31 +253,44 @@ public class AddCoursesTests{
         void emptyCourseNameGivesWarning() {
             addCoursePage.enterCourseName("");
             addCoursePage.clickSubmit();
-            Assertions.assertTrue(addCoursePage.getEmptyErrorMessage());
+            System.out.println(addCoursePage.getEmptyErrorMessage());
+            Assertions.assertEquals("Please fill in this field.", addCoursePage.getEmptyErrorMessage());
         }
 
         @Test
         @DisplayName("non-empty course name does not give a warning")
         void nonEmptyCourseNameNoWarning() {
             addCoursePage.enterCourseName("Eng-92");
-            addCoursePage.clickSubmit();
-            Assertions.assertFalse(addCoursePage.getEmptyErrorMessage());
+            Assertions.assertEquals("Please fill in this field.", addCoursePage.getEmptyErrorMessage());
         }
 
         @Test
         @DisplayName("Blank course name gives warning")
         void blankCourseNameGivesWarning() {
             addCoursePage.enterCourseName("   ");
-            addCoursePage.clickSubmit();
-            Assertions.assertTrue(addCoursePage.getEmptyErrorMessage());
+            Assertions.assertEquals("true", addCoursePage.getEmptyErrorMessage());
         }
     }*/
 
     @Nested
-    @DisplayName("Valid Entries test")
+    @DisplayName("Alert test for number of trainers entries")
     class ValidEntriesTest {
 
+        @Test
+        @DisplayName("Max value reached alert for above max number of trainers entry")
+        void isAlertForMaxNumber() {
+            addCoursePage.enterNumberOfTrainers(11);
+            addCoursePage.clickSubmit();
+            Assertions.assertTrue(addCoursePage.isAlertDisplayed());
+        }
 
+        @Test
+        @DisplayName("Min value reached alert for above max number of trainers entry")
+        void isAlertForMinNumber() {
+            addCoursePage.enterNumberOfTrainers(0);
+            addCoursePage.clickSubmit();
+            Assertions.assertTrue(addCoursePage.isAlertDisplayed());
+        }
     }
 
     @AfterEach
