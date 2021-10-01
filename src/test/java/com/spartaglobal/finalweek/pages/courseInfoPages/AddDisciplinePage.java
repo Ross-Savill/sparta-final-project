fcc298b559a9c2d6a6cd7d5433c7d804a6ba09f0
joshuamc360 <joshuamc360@gmail.com>
2021-10-01 10:14:54 +0100

    Discipline page and test added. Not implemented with courseInfoPage.

--- src/test/java/com/spartaglobal/finalweek/pages/courseInfoPages/AddDisciplinePage.java
+++ src/test/java/com/spartaglobal/finalweek/pages/courseInfoPages/AddDisciplinePage.java
@@ -2,59 +2,81 @@ package com.spartaglobal.finalweek.pages.courseInfoPages;
 
 import com.spartaglobal.finalweek.interfaces.URLable;
 import com.spartaglobal.finalweek.pages.NavTemplate;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.w3c.dom.html.HTMLInputElement;
 
 public class AddDisciplinePage extends NavTemplate implements URLable {
 
-    @FindBy(id = "discipline-name") WebElement disciplineName;
-    @FindBy(id = "discipline-duration") WebElement duration;
+    @FindBy(id = "discipline-name") WebElement disciplineNameTextField;
+    @FindBy(id = "discipline-duration") WebElement durationTextField;
     @FindBy(id = "btn-primary") WebElement submitButton;
 
     public AddDisciplinePage() {
         PageFactory.initElements(webDriver, this);
     }
 
-//    public void enterDisciplineName(String name) {
-//
-//    }
+    public String getDisciplineNameTextField() {
+        return webDriver.findElement(By.id("disciplineNameTextField")).getAttribute("value");
+    }
 
-//    public void enterDuration(int duration) {
-//
-//    }
+    public String getDurationTextField() {
+        return webDriver.findElement(By.id("durationTextField")).getAttribute("value");
+    }
 
-//    public boolean submitSuccessful() {
-//
-//    }
+    public void enterDisciplineName(String name) {
+        disciplineNameTextField.sendKeys(name);
+    }
 
-//    public CourseInfoPage goToCourseInfoPage() {
-//
-//    }
+    public void enterDuration(String duration) {
+        durationTextField.sendKeys(duration);
+    }
 
-//    public boolean isDisciplineNameValid() {
-//
-//    }
+    public boolean submitSuccessful() {
+        submitButton.click();
+        return submitButton.isEnabled();
+    }
 
-//    public boolean isDisciplineDurationValid() {
-//
-//    }
+    public CourseInfoPage goToCourseInfoPage() {
+        courseInfoPageLink.click();
+        return new CourseInfoPage();
+    }
 
-//    public boolean areAllFieldsValid() {
-//
-//    }
+    public boolean isDisciplineNameValid() {
+        String durationTextFieldAttribute = durationTextField.getAttribute("value");
+        String durationReg = "[AZ]{20}";
+        return (durationTextFieldAttribute.matches(durationReg));
+    }
 
-//    public boolean isDisciplineNameEmpty() {
-//
-//    }
+    public boolean isDisciplineDurationValid() {
+        int maxDuration = 1000;
+        String durationTextFieldAttribute = durationTextField.getAttribute("value");
+        int durationInt = Integer.parseInt(durationTextFieldAttribute);
+        return (durationInt<=maxDuration);
+    }
 
-//    public boolean isDisciplineDurationEmpty() {
-//
-//    }
+    public boolean areAllFieldsValid() {
+        return (isDisciplineDurationValid()&&isDisciplineNameValid());
+    }
 
-//    public boolean areAllFieldsEmpty() {
-//
-//    }
+    public boolean isDisciplineNameEmpty() {
+        String disciplineTextFieldAttribute = disciplineNameTextField.getAttribute("value");
+        return (disciplineTextFieldAttribute.isEmpty());
+    }
+
+    public boolean isDisciplineDurationEmpty() {
+        String durationTextFieldAttribute = durationTextField.getAttribute("value");
+        return (durationTextFieldAttribute.isEmpty());
+    }
+
+    public boolean areAllFieldsEmpty() {
+        String disciplineTextFieldAttribute = disciplineNameTextField.getAttribute("value");
+        String durationTextFieldAttribute = durationTextField.getAttribute("value");
+
+        return (disciplineTextFieldAttribute.isEmpty()&&durationTextFieldAttribute.isEmpty());
+    }
 
     @Override
     public String getURL() {
--- src/test/java/com/spartaglobal/finalweek/tests/courseInfoTests/AddDisciplineTests.java
+++ src/test/java/com/spartaglobal/finalweek/tests/courseInfoTests/AddDisciplineTests.java
@@ -1,14 +1,95 @@
 package com.spartaglobal.finalweek.tests.courseInfoTests;
 
+import com.spartaglobal.finalweek.base.TestBase;
+import com.spartaglobal.finalweek.pages.LoginPage;
 import com.spartaglobal.finalweek.pages.NavTemplate;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.spartaglobal.finalweek.pages.SchedulerPage;
+import com.spartaglobal.finalweek.pages.courseInfoPages.AddDisciplinePage;
+import com.spartaglobal.finalweek.pages.courseInfoPages.CourseInfoPage;
+import com.spartaglobal.finalweek.util.PropertiesLoader;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class AddDisciplineTests extends NavTemplate {
 
+    private static final String userName = PropertiesLoader.getProperties().getProperty("Username");
+    private static final String password =PropertiesLoader.getProperties().getProperty("Password");
+
+    private LoginPage loginPage;
+    private SchedulerPage schedulerPage;
+    private CourseInfoPage courseInfoPage;
+    private AddDisciplinePage addDisciplinePage;
+
     @BeforeEach
     public void setup() {
+        TestBase.initialisation();
 
+        loginPage = new LoginPage();
+        schedulerPage = loginPage.login(userName,password);
+
+        schedulerPage = new SchedulerPage();
+        NavTemplate navTemplate = new NavTemplate();
+
+        courseInfoPage = schedulerPage.goToCourseInfoPage();
+        courseInfoPage = new CourseInfoPage();
+
+        addDisciplinePage = courseInfoPage.goToAddDisciplinePage();
+        addDisciplinePage = new AddDisciplinePage();
+
+    }
+
+    @Test
+    @DisplayName("Check if discipline field can be passed a value.")
+    void testDisciplineField(){
+        String newDiscipline = "Ruby";
+        addDisciplinePage.enterDisciplineName(newDiscipline);
+        Assertions.assertEquals(newDiscipline,addDisciplinePage.getDisciplineNameTextField());
+    }
+
+    @Test
+    @DisplayName("Check if duration field can be passed a value.")
+    void testDurationField(){
+        String duration = "13";
+        addDisciplinePage.enterDuration(duration);
+        Assertions.assertEquals(duration,addDisciplinePage.getDurationTextField());
+    }
+
+    @Test
+    @DisplayName("Check Submission Button is Clickable.")
+    void testSubmissionIsClicked(){
+        Assertions.assertTrue(addDisciplinePage.submitSuccessful());
+    }
+
+    @ParameterizedTest
+    @ValueSource (strings = {"Ruby", "Python12", "Java", "R2 D2"})
+    @DisplayName("Discipline name must be letters only.")
+    void testFieldIsLettersOnly(String names){
+        addDisciplinePage.enterDisciplineName(names);
+        Assertions.assertTrue(addDisciplinePage.isDisciplineNameValid());
+    }
+
+    @ParameterizedTest
+    @ValueSource (strings = {"200", "1", "999", "1000"})
+    @DisplayName("Duration Field must be numbers only.")
+    void testFieldIsNumbersOnly(){
+        Assertions.assertTrue(addDisciplinePage.submitSuccessful());
+    }
+
+    @Test
+    @ParameterizedTest
+    @ValueSource (strings = {"-1", "0", "1", "1000", "1001"})
+    @DisplayName("Duration Field must less than or equal to 1000.")
+    void testFieldIsLessThanOneThousand(){
+
+    }
+
+    @Test
+    @DisplayName("Duration Field must less than or equal to 1000.")
+    void testFieldAreEmpty(){
+        addDisciplinePage.enterDuration("");
+        addDisciplinePage.enterDisciplineName("");
+        Assertions.assertTrue(addDisciplinePage.areAllFieldsEmpty());
     }
 
     @AfterEach
