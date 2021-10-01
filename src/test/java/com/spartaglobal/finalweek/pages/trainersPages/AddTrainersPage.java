a00c95bd6f1d9f9d7702044e4e0695dd77ea2be8
Yrahman1 <yrahman@spartaglobal.com>
2021-10-01 10:39:43 +0100

    Adding Add and Edit trainer class with repective tests

--- src/test/java/com/spartaglobal/finalweek/pages/trainersPages/AddTrainersPage.java
+++ src/test/java/com/spartaglobal/finalweek/pages/trainersPages/AddTrainersPage.java
@@ -4,84 +4,119 @@ import com.spartaglobal.finalweek.interfaces.URLable;
 import com.spartaglobal.finalweek.pages.NavTemplate;
 import com.spartaglobal.finalweek.pages.SchedulerPage;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class AddTrainersPage extends NavTemplate implements URLable {
 
-    @FindBy(id = "username") WebElement usernameInputField;
-    @FindBy(id = "password") WebElement passwordInputField;
-    @FindBy(className = "btn-primary") WebElement submitButton;
+    @FindBy(id = "trainer-first-name")
+    WebElement firstNameTextBox;
+    @FindBy(id = "trainer-last-name")
+    WebElement lastNameTextBox;
+    @FindBy(id = "trainer-hex")
+    WebElement colourPicker;
+    @FindBy(className = "btn btn-primary")
+    WebElement submitButton;
+    @FindBy(id = "trainerPageLink")
+    WebElement trainersPageButton;
 
 
     public AddTrainersPage() {
         PageFactory.initElements(webDriver, this);
     }
 
-//    public WebElement getUsernameElement() {
-//
-//    }
-
-//    public WebElement getPasswordElement() {
-//
-//    }
-
-//    public String getUsername() {
-//
-//    }
-
-//    public String getPassword() {
-//
-//    }
-
-//    public void enterUsername(String username) {
-//
-//    }
-
-//    public void enterPassword(String password) {
-//
-//    }
-
-//    public SchedulerPage login(String username, String password) {
-//
-//    }
-
-//    public SchedulerPage clickLoginButton() {
-//
-//    }
-
-//    public WebElement getErrorMessage() {
-//
-//    }
-
-//    public boolean isUsernameFieldEmpty() {
-//
-//    }
-
-//    public boolean isPasswordFieldEmpty() {
-//
-//    }
-
-//    public boolean areAllFieldsEmpty() {
-//
-//    }
-
-//    public boolean isUsernameValid() {
-//
-//    }
-
-//    public boolean isPasswordValid() {
-//
-//    }
-
-//    public boolean areAllFieldsEmpty() {
-//
-//    }
-
     @Override
     public String getURL() {
         return webDriver.getCurrentUrl();
     }
+
+    public String getFirstName(){
+        return firstNameTextBox.getAttribute("value");
+    }
+
+    public void enterFirstName(String firstName){
+        firstNameTextBox.sendKeys(firstName);
+    }
+
+    public String getLastName(){
+        return lastNameTextBox.getAttribute("value");
+    }
+
+    public void enterLastName(String lastName){
+        lastNameTextBox.sendKeys(lastName);
+    }
+
+    public String getColour(){
+        return colourPicker.getAttribute("value");
+    }
+
+    public void setColour(){
+        JavascriptExecutor jse=(JavascriptExecutor)webDriver;
+        jse.executeScript("document.getElementByid('EditorColorPicker1').value='#FFEEXX'");
+    }
+
+    public void submitTrainer()
+    {
+        submitButton.click();
+    }
+
+    public TrainersPage goToTrainersPage(){
+        trainersPageButton.click();
+        return new TrainersPage();
+    }
+
+    public boolean isSubmitSuccessful(){
+        submitTrainer();
+        if(getURL() == trainersPageURL ) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFirstNameEmpty(){
+        if(getFirstName() == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isLastNameEmpty(){
+        if(getLastName() == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean areAllFieldsEmpty(){
+        if(isFirstNameEmpty() == true && isLastNameEmpty() == true ) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFirstNameValid(){
+        if(getFirstName().matches("/^[a-z 0-9 ,.'-]+$/i")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isLastNameValid(){
+        if(getLastName().matches("/^[a-z 0-9 ,.'-]+$/i")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean areAllFieldsValid(){
+        if(isFirstNameValid() == true && isLastNameValid() == true ) {
+            return true;
+        }
+        return false;
+    }
+
 
 }
