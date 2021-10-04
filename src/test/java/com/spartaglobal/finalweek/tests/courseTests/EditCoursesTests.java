8239f6ea930a478d003d685a1d256b9a4837940a
Sogurung16 <Sogurung16@gmail.com>
2021-10-04 10:18:00 +0100

    edit page tests setup

--- src/test/java/com/spartaglobal/finalweek/tests/courseTests/EditCoursesTests.java
+++ src/test/java/com/spartaglobal/finalweek/tests/courseTests/EditCoursesTests.java
@@ -1,14 +1,40 @@
 package com.spartaglobal.finalweek.tests.courseTests;
 
+import com.spartaglobal.finalweek.base.TestBase;
+import com.spartaglobal.finalweek.pages.LoginPage;
 import com.spartaglobal.finalweek.pages.NavTemplate;
+import com.spartaglobal.finalweek.pages.coursePages.CoursePage;
+import com.spartaglobal.finalweek.pages.coursePages.EditCoursesPage;
+import com.spartaglobal.finalweek.pages.coursePages.ModifyCoursePage;
+import com.spartaglobal.finalweek.util.PropertiesLoader;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 public class EditCoursesTests extends NavTemplate {
+    private LoginPage loginPage;
+    private NavTemplate navTemplate;
+    private CoursePage coursePage;
+    private EditCoursesPage editCoursePage;
 
     @BeforeEach
     public void setup() {
+        TestBase.initialisation();
+        loginPage = new LoginPage();
 
+        loginPage.login(
+                PropertiesLoader.getProperties().getProperty("Username"),
+                PropertiesLoader.getProperties().getProperty("Password")
+        );
+
+        navTemplate = new NavTemplate();
+
+        coursePage = navTemplate.goToCoursesPage();
+        coursePage = new CoursePage();
+
+        //TODO: Delete all courses on courses page.
+
+        /*editCoursePage = coursePage.clickEditCourseButton();
+        editCoursePage = new ModifyCoursePage();*/
     }
 
     @AfterEach
