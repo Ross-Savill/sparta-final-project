9c42efc5c33019637a66c0909bc7605a931bcd24
nirel-warde <nirelwarde@gmail.com>
2021-10-04 23:00:05 +0100

    all test methods for AddQualityGatePage

--- src/test/java/com/spartaglobal/finalweek/tests/traineeTests/AddQualityGateTests.java
+++ src/test/java/com/spartaglobal/finalweek/tests/traineeTests/AddQualityGateTests.java
@@ -1,8 +1,19 @@
 package com.spartaglobal.finalweek.tests.traineeTests;
 
 import com.spartaglobal.finalweek.pages.NavTemplate;
+import com.spartaglobal.finalweek.pages.traineePages.TraineesPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 
 public class AddQualityGateTests extends NavTemplate {
 
@@ -10,6 +21,187 @@ public class AddQualityGateTests extends NavTemplate {
     public void setup() {
 
     }
+
+
+    @Test
+    @DisplayName("enter Trainee Id")
+    void enterTraineeId() {
+
+    }
+
+    @Test
+    @DisplayName("get Trainee Id Element")
+    void getTraineeIdElement() {
+
+    }
+
+    @Test
+    @DisplayName("get Trainee Id")
+    void getTraineeId() {
+
+    }
+
+
+    @Test
+    @DisplayName("set Quality Gate Status")
+    void setQualityGateStatus() {
+
+    }
+
+    @Test
+    @DisplayName("click Quality Gate Drop Down")
+    void clickQualityGateDropDown() {
+
+    }
+
+    @Test
+    @DisplayName("get Quality Gate Status Element")
+    void getQualityGateStatusElement() {
+
+    }
+//
+//    @Test
+//    @DisplayName("get Quality Gate Status Element")
+//    void getQualityGateStatusElement() {
+//
+//    }
+
+    @Test
+    @DisplayName("get Quality Gate Status")
+    void getQualityGateStatus() {
+
+    }
+
+    @Test
+    @DisplayName("set Trainer One Id")
+    void setTrainerOneId() {
+
+    }
+
+    @Test
+    @DisplayName("get Trainer One Id")
+    void getTrainerOneId() {
+
+    }
+
+
+    @Test
+    @DisplayName("enter Trainer One Feedback")
+    void enterTrainerOneFeedback() {
+
+    }
+
+    @Test
+    @DisplayName("get Trainer One Feedback")
+    void getTrainerOneFeedback() {
+
+    }
+
+    @Test
+    @DisplayName("set Trainer Two Id")
+    void setTrainerTwoId() {
+
+    }
+
+    @Test
+    @DisplayName("enter Trainer Two Feedback")
+    void enterTrainerTwoFeedback() {
+
+    }
+
+    @Test
+    @DisplayName("get Trainer Two Feedback")
+    void getTrainerTwoFeedback() {
+
+    }
+
+
+    @Test
+    @DisplayName("get Trainer Two Id")
+    void getTrainerTwoId() {
+
+    }
+
+
+    @Test
+    @DisplayName("set Date")
+    void setDate() {
+
+    }
+
+    @Test
+    @DisplayName("get Date")
+    void getDate() {
+
+    }
+
+
+    @Test
+    @DisplayName("go To Trainees Page")
+    void goToTraineesPage() {
+
+    }
+
+
+    @Test
+    @DisplayName("is Submit Successful")
+    void isSubmitSuccessful() {
+
+    }
+
+    @Test
+    @DisplayName("is Trainer One Feedback Empty")
+    void isTrainerOneFeedbackEmpty() {
+
+    }
+
+
+
+
+    @Test
+    @DisplayName("is Trainer Two Feedback Empty")
+    void isTrainerTwoFeedbackEmpty() {
+
+    }
+
+    @Test
+    @DisplayName("Is Valid")
+    void isValid() {
+
+    }
+
+    @Test
+    @DisplayName("is Date Valid")
+    void isDateValid() {
+
+    }
+
+    @Test
+    @DisplayName("is Date Empty")
+    void isDateEmpty() {
+
+    }
+
+
+    @Test
+    @DisplayName("areAllFielsValid")
+    void areAllFielsValid() {
+
+    }
+
+    @Test
+    @DisplayName("are All Fields Empty")
+    void areAllFieldsEmpty() {
+
+    }
+
+
+    @Test
+    @DisplayName("get Url")
+    void getUrl() {
+
+    }
+
 
     @AfterEach
     public void tearDown(){
